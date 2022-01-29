1c59b560b291a9e20abab0c6473927ef94568c71
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-29 19:31:13 +0530

    added comments

--- LinearDS/LinkedList/c-cpp/linkedList.cpp
+++ LinearDS/LinkedList/c-cpp/linkedList.cpp
@@ -1,7 +1,9 @@
 /*
 "node" is a data type created using struct; which contains
-integer 'data' & node 'next' .
+integer 'data' & node 'next' 
 (can be seen at ./linkedListReversal.cpp @ line 4)
+
+linkedListReversal is a function imported from ./linkedListReversal; which executes reversal of linked list using recursion method
 */
 
 #include <iostream>
