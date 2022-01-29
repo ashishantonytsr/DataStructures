2720888d203f86043a8d0890a77c40765809135e
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-29 13:40:35 +0530

    removed unwanted comments

--- SortingAlgos/MergeSort/python/MergeSort.py
+++ SortingAlgos/MergeSort/python/MergeSort.py
@@ -8,23 +8,19 @@ def merge(List, start, end, mid):
 	while i<=mid and j<=end:
 		if List[i] <= List[j]:
 			temp.append(List[i])
-			# k+=1
 			i+=1
 		else:
 			temp.append(List[j])
-			# k+=1
 			j+=1
 		
 	# adding rest from left subList to temp
 	while i<=mid:
 		temp.append(List[i])
-		# k+=1
 		i+=1
 	
 	# adding rest from right subList to temp
 	while j<=end:
 		temp.append(List[j])
-		# k+=1
 		j+=1
 
 	# adding items from temp to List
