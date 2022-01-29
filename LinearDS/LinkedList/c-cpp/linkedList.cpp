24d4a83b6a147680a24eb51816bdb33aaea17923
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-29 19:12:54 +0530

    added reversal, updated insertion, display

--- LinearDS/LinkedList/c-cpp/linkedList.cpp
+++ LinearDS/LinkedList/c-cpp/linkedList.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 #include <stdlib.h>
+#include "LinkedListReversal.cpp"
 using namespace std;
 
-struct node
-{
-	int data;
-	node *nextAddress; // address to next node; which is of datatype, node
-};
-node *head = NULL, *current = NULL;
-
-// TODO: understand the functionality
-void displayList()
+void displayList(node *head)
 {
 	node *index = head;
 	while (index != NULL)
 	{
 		cout << index->data << '\n';
-		index = index->nextAddress;
+		index = index->next;
 	}
 }
 
-void insertFirst(int data)
+node *insertFirst(node *head, int data)
 {
 	node *link;
 	link = (node *)malloc(sizeof(node)); // Memory allocated with data type & size (sizeof(data_type))
 	link->data = data;
-	link->nextAddress = head;
+	link->next = head;
 	head = link;
+	return head;
 }
 
 // TODO: implement deletion
@@ -40,12 +34,15 @@ void deleteFirst(int item)
 
 int main()
 {
-	insertFirst(5);
-	insertFirst(10);
-	insertFirst(15);
-	insertFirst(40);
-	// insertFirst(20);
-	// insertFirst(40);
-	displayList();
+	node *head = NULL;
+	head = insertFirst(head, 5);
+	head = insertFirst(head, 10);
+	head = insertFirst(head, 15);
+	head = insertFirst(head, 40);
+
+	// displayList(head);
+	node *reversedHead;
+	reversedHead = linkedListReversal(head);
+	displayList(reversedHead);
 	return 0;
 }
