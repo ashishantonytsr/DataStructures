dad24bddce0e2f02849b220435e1915ca4e42bd0
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-01-29 22:48:16 +0530

    added tests for sorting

--- SortingAlgos/MergeSort/cpp/test.cpp
+++ SortingAlgos/MergeSort/cpp/test.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 #include "MergeSort.cpp"
 using namespace std;
+
+bool isSorted(int test[], int len)
+{
+	for (int i = 1; i < len - 1; i++)
+	{
+		int left = test[i - 1], right = test[i + 1];
+		if ((test[i] < left || test[i] > right))
+			return false;
+	}
+	return true;
+}
+
 int main()
 {
-
-	int testArray[] = {9, 8, 7, 5, 6, 3, 2, 1, 0, 4};
-	mergeSort(testArray, 0, 9);
-	cout << "Sorted Array : ";
-	for (int i = 0; i <= 9; i++)
-	{
-		cout << testArray[i] << " ";
-	}
-	return 0;
+	srand(time(NULL));
+	int arr[100];
+	// populating array with random values
+	for (int i = 0; i < 100; i++)
+		arr[i] = rand() % 1000;
+	mergeSort(arr, 0, 99);
+	// testing...
+	isSorted(arr, 100) == 1 ? cout << "\ntest passed" : cout << "\ntest failed";
 }
