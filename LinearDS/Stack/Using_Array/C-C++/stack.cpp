1c1262753c04606707df36955309a0ee07740680
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-05-03 00:29:15 +0530

    Relocated files

--- LinearDS/Stack/Using_Array/C-C++/stack.cpp
+++ LinearDS/Stack/Using_Array/C-C++/stack.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
 
-int endPointer = -1;
+int TOP = -1;
 
-void display(int stack[]) {
-	if (endPointer == -1)
+void display(int stack[])
+{
+	if (TOP == -1)
 		std::cout << "\n\tStack Empty";
-	else {
+	else
+	{
 		std::cout << std::endl
 							<< '\t';
-		for (int i = 0; i <= endPointer; i++)
+		for (int i = 0; i <= TOP; i++)
 			std::cout << stack[i] << " ";
 	}
 	std::cout << std::endl;
 }
 
-void push(int stack[], int stackSize, int item) {
-	if (endPointer < stackSize - 1) {
-		endPointer++;
-		stack[endPointer] = item;
+void push(int stack[], int stackSize, int item)
+{
+	if (TOP < stackSize - 1)
+	{
+		TOP++;
+		stack[TOP] = item;
 		std::cout << "Pushed item : " << item << std::endl;
-	} else {
+	}
+	else
+	{
 		std::cout << "\n\tError : Stack OverFlow";
 	}
 }
 
-void pop(int stack[]) {
-	if (endPointer < 0)
+void pop(int stack[])
+{
+	if (TOP < 0)
 		std::cout << "\n\tError : Stack UnderFlow";
-	else {
-		int poppedItem = stack[endPointer];
+	else
+	{
+		int poppedItem = stack[TOP];
 		std::cout << "Popped item " << poppedItem << std::endl;
-		endPointer--;
+		TOP--;
 	}
 }
 
-void search(int stack[], int item) {
-	for (int i = 0; i <= endPointer; i++)
-		if (item == stack[i]) {
+void search(int stack[], int item)
+{
+	for (int i = 0; i <= TOP; i++)
+		if (item == stack[i])
+		{
 			std::cout << item << " found at location " << i + 1;
 			return;
 		}
